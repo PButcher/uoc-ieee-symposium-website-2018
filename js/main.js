a002e0fac6d8064f270d99351769d3760caac5e5
pbutcher <pbutcher93@gmail.com>
2018-06-28 02:22:12 +0100

    Latest changes from last meeting

---
+++ js/main.js
@@ -0,0 +1,1 @@
+(function(){Array.from(document.getElementById('drawer').getElementsByTagName('a')).forEach(el=>{el.addEventListener('click',()=>{document.getElementById('burger').checked = false;});});})();
