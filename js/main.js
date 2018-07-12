d78e781bd8d69ba6c289e248da3ce6fe05dfc25c
pbutcher <pbutcher93@gmail.com>
2018-07-12 21:06:12 +0100

    First live version

--- js/main.js
+++
@@ -1,1 +0,0 @@
-(function(){Array.from(document.getElementById('drawer').getElementsByTagName('a')).forEach(el=>{el.addEventListener('click',()=>{document.getElementById('burger').checked = false;});});})();
